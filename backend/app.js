9bd9fa3bd9027ddc589aff13141585d0c9a11a15
Lina Lubig <lina.lubig@dci.education>
2023-07-12 14:07:00 +0200

    mongoDB roles

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,20 @@ const mongoCluster = process.env.MONGO_CLUSTER;
 const mongoDatabase = process.env.MONGO_DATABASE;
 const mongoRoles = process.env.MONGO_ROLES;
 
+mongoose
+    .connect(`mongodb+srv://${mongoUser}:${mongoPwd}@${mongoCluster}/${mongoDatabase}?${mongoRoles}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
