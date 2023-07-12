b05fff20227407e3a5a844eb84e6145c48828838
Lina Lubig <lina.lubig@dci.education>
2023-07-12 14:04:38 +0200

    mongoDB roles for .env

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+
+const mongoUser = process.env.MONGO_USER;
+const mongoPwd = process.env.MONGO_PWD;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoDatabase = process.env.MONGO_DATABASE;
+const mongoRoles = process.env.MONGO_ROLES;
 
 const app = express();
 
