56efdd03dc2d8751f4f95746c4a40043baafeeef
Lina Lubig <lina.lubig@dci.education>
2023-07-13 14:18:28 +0200

    getSingleUser, upDateUser

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,8 @@
-const { createUser } = require("../model/user.model");
+const {
+    createUser,
+    getSingleUser,
+    updateUser,
+} = require("../model/user.model");
 
 async function httpCreateUser(req, res, next) {
     try {
@@ -9,5 +13,23 @@ async function httpCreateUser(req, res, next) {
         next(error);
     }
 }
+async function httpGetSingleUser(req, res, next) {
+    try {
+        const { id } = req.params;
+        const user = await getSingleUser(id);
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
+}
+async function httpUpdateUser(req, res, next) {
+    try {
+        const { id } = req.params;
+        const updatedUser = await updateUser(id, req.body);
+        res.json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+}
 
-module.exports = { httpCreateUser };
+module.exports = { httpCreateUser, httpGetSingleUser, httpUpdateUser };
